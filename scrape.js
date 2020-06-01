035590c3447fd4dbb0925d7a31e136d036904aa3
abidurr <42abid@gmail.com>
2020-05-31 20:36:03 -0400

    Scrape

--- scrape.js
+++ scrape.js
@@ -1,2 +1,8 @@
 const request = require("request");
 const cheerio = require("cheerio");
+
+request("https://write.as/matt", (error, response, html) => {
+  if (!error && response.statusCode == 200) {
+      console.log(html);
+  }
+});
