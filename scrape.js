0f6c1f541e949b48459cca2057be179b62f6a226
abidurr <42abid@gmail.com>
2020-05-31 20:55:33 -0400

    Looping

--- scrape.js
+++ scrape.js
@@ -4,7 +4,11 @@ const cheerio = require("cheerio");
 request("https://write.as/matt", (error, response, html) => {
   if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
-      const siteHeading = $(".post-title");
-      console.log(siteHeading.text())
+      // const siteHeading = $(".post-title");
+      // console.log(siteHeading.text())
+      $(".post-title").each((i, el) => {
+          const item = $(el).text();
+          console.log(item);
+      })
   }
 });
